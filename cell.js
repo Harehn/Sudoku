b9288b72081284e92f48a4b88bf5eb2f27a2d04b
Harehn <harehn.kaundun@mail.mcgill.ca>
2018-08-04 20:44:37 -0400

    initialised the sudoku

--- cell.js
+++ cell.js
@@ -1,13 +1,22 @@
 class cell{
-	constructor(x,y,length){
+	constructor(x,y,length,number){
 		this.x=x;
 		this.y=y;
 		this.length=length;
 		this.show();
+		number?this.number=number:{};
 	}
 	show(){
-		context.strokeStyle="black"
-		context.lineWidth=6;
+		context.strokeStyle="black";
+		context.fillStyle="black";
+		context.lineWidth=1;
 		context.rect(this.x,this.y,this.length,this.length);
+		context.stroke();
+	}
+	write(num){
+	context.fillStyle="black";
+	context.font="30px Arial";
+	context.fillText(num+"",this.x+(this.length/3),this.y+(this.length*2/3));
+	this.number?{}:this.number=num;
 	}
 }
