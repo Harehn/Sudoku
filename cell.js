469b551e460f9b53bd017e1088d0ab79a56d6bfe
Harehn <harehn.kaundun@mail.mcgill.ca>
2018-08-08 15:24:43 -0400

    Added columns and rows

--- cell.js
+++ cell.js
@@ -5,20 +5,39 @@ class cell{
 		this.i=x/length;
 		this.j=y/length;
 		this.length=length;
+		this.sorted=false;
+		this.highlighted=false;
 		this.show();
+		this.group=null;
 		number?this.number=number:{};
 	}
 	show(){
 		context.strokeStyle="black";
-		context.fillStyle="black";
+		if(this.highlighted){
+			context.fillStyle="rgb(255, 251, 204)";
+			context.fillRect(this.x,this.y,this.length,this.length);
+		}
 		context.lineWidth=1;
+		context.beginPath();
 		context.rect(this.x,this.y,this.length,this.length);
 		context.stroke();
+		if(this.number){
+			this.write(this.number);
+			console.log(this.i,this.j);
+		}
 	}
 	write(num){
 	context.fillStyle="black";
 	context.font="30px Arial";
 	context.fillText(num+"",this.x+(this.length/3),this.y+(this.length*2/3));
 	this.number?{}:this.number=num;
+	}
+	sort(){
+		this.highlighted=true;
+	}
+	swapWith(AnotherCell){
+		let temp=AnotherCell.number;
+		AnotherCell.number=this.number;
+		this.number=temp;
 	}
 }
--- groups.js
+++ groups.js
@@ -1,10 +1,28 @@
 
+function make2DArray(row,col){
+	let arr=new Array(row);
+	for(let i=0;i<row;i++){
+		arr[i]=new Array(col);
+	}
+	return arr;
+}
+
+function transpose2DArray(row,col,arr){
+	let transpose=make2DArray(col,row);
+	for(let i=0;i<row;i++){
+		for(let j=0;j<col;j++){
+			transpose[j][i]=arr[i][j];
+		}
+	}
+	return transpose;
+}
 function makeGroup(x1,y1,x2,y2,allCells){
 	let cells=new Array(9);
 	let k=0;
 	for(let i=x1;i<x2+1;i++){
 		for(let j=y1;j<y2+1;j++){
 			cells[k]= allCells[i] [j];
+			cells[k].group=cells;
 			k++;
 		}
 	}
